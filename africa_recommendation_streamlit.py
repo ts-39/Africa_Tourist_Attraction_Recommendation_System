c8eddef231e1644d524802b7986fd2e8800d6ab1
ts-39 <111523486+ts-39@users.noreply.github.com>
2024-06-23 18:43:12 +0900

    Update africa_recommendation_streamlit.py

--- africa_recommendation_streamlit.py
+++ africa_recommendation_streamlit.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 # import seaborn as sns
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 city_names = [
     "Bangkok", "Paris", "London", "Dubai", "Singapore", "Kuala Lumpur",
